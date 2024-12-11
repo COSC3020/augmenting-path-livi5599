4dbb2386654cbdb57e90e62d4ece4db176a46319
livi5599 <obrown1@uwyo.edu>
2024-12-10 23:44:09 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function augmentingPath(graph, start, end) {
+    if (start == end) {
+        return [start];
+    }
+    if (!(start in graph) || !(end in graph)) {
+        return [];
+    }
+    var unvisited = [];
+    var path = [];
+    let keys = Object.keys(graph);
+    console.log(keys);
+    for (node = 0; node < keys.length; node++) {
+        unvisited.push(graph[node]);
+    }
+    //console.log(unvisited);
     return [];
 }
